3408654741889dc982934741cdff57f3371efacb
lucasrenandns <lucasrenandns96@gmail.com>
2023-03-04 15:01:02 -0300

    Quiz App

---
+++ js/questions.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        question: "Qual o significado da sigla HTML?",
+        answers: [
+            {option: "Hipertexto de marcação de linguagem", correct: false},
+            {option: "Marcação de linguagem de Hipertexto", correct: false},
+            {option: "Texto de marcação de linguagem", correct: false},
+            {option: "Linguagem de marcação de Hipertexto", correct: true},
+        ]
+    },
+    {
+        question: "Qual o significado da sigla CSS?",
+        answers: [
+            {option: "Folhas de estilo em cascata", correct: true},
+            {option: "Estilo de folhas em cascata", correct: false},
+            {option: "Cascata de folhas em estilo", correct: false},
+            {option: "Cascata de estilo em folhas", correct: false},
+        ]
+    },
+    {
+        question: "Qual das alternativas abaixo não é considerada uma linguagem de programação?",
+        answers: [
+            {option: "C/C++", correct: false},
+            {option: "Ruby/GO", correct: false},
+            {option: "Html/Css", correct: true},
+            {option: "Php/Swift", correct: false},
+        ]
+    },
+    {
+        question: "Qual a linguagem de programação nativa dos Browsers(Navegadores Web)?",
+        answers: [
+            {option: "Java", correct: false},
+            {option: "C#", correct: false},
+            {option: "Python", correct: false},
+            {option: "JavaScript", correct: true},
+        ]
+    },
+    {
+        question: "Defina o que é JavaScript?",
+        answers: [
+            {option: "É uma linguagem de programação", correct: true},
+            {option: "É uma linguagem de Marcação", correct: false},
+            {option: "É uma linguagem de estilização", correct: false},
+            {option: "Nenhuma das alternativas", correct: false},
+        ]
+    },
+    {
+        question: "Qual a função do JavaScript?",
+        answers: [
+            {option: "Adicionar velocidade as páginas web", correct: false},
+            {option: "Adicionar interatividade as páginas web", correct: true},
+            {option: "Criar páginas web", correct: false},
+            {option: "Adicionar estilo as páginas web", correct: false},
+        ]
+    },
+]
---
+++ js/script.js
@@ -0,0 +1,85 @@
+// Modo estrito
+"use strict"
+
+// Elementos
+const startQuiz = document.getElementById("start")
+const mainContainer = document.getElementById("main")
+const questionsAmount = document.getElementById("questions-amount")
+const amount = document.getElementById("amount")
+const questionTitle = document.getElementById("question-title")
+const title = document.getElementById("title")
+const questionsButton = document.getElementById("questions")
+const exitContent = document.getElementById("exit-content")
+const exitText = document.getElementById("exit-text")
+const refresh = document.getElementById("refresh")
+
+import questions from "./questions.js"
+
+let currentIndex = 0
+let questionCorrect = 0
+
+// Iniciar quiz
+startQuiz.addEventListener("click", () => {
+    mainContainer.style.display = "flex"
+    questionsAmount.style.display = "block"
+    questionTitle.style.display = "block"
+    questionsButton.style.display = "block"
+    startQuiz.style.display = "none"
+})
+
+// Reiniciar quiz
+refresh.addEventListener("click", () => {
+    exitContent.style.display = "none"
+    startQuiz.style.display = "block"
+    mainContainer.style.display = "none"
+
+    currentIndex = 0
+    questionCorrect = 0
+    loadQuestions()
+})
+
+// Próxima questão
+function nextQuestion(event) {
+    if(event.target.getAttribute("data-correct") === "true") {
+        questionCorrect ++
+    }
+    if(currentIndex < questions.length - 1) {
+        currentIndex ++
+        loadQuestions()
+    }
+    else {
+        finish()
+    }
+}
+
+// Resultado final
+function finish() {
+    exitText.textContent = `Você acertou ${questionCorrect} de ${questions.length} questões!`
+    questionsAmount.style.display = "none"
+    questionsButton.style.display = "none"
+    questionTitle.style.display = "none"
+    exitContent.style.display = "flex"
+}
+
+// Carregar questões
+function loadQuestions() {
+    amount.textContent = `${currentIndex + 1} / ${questions.length}`
+    
+    const questionsItems = questions[currentIndex]
+    questionsButton.textContent = ""
+    title.textContent = questionsItems.question
+    
+    questionsItems.answers.forEach((answer) => {
+        const buttonElement = document.createElement("button")
+        
+        buttonElement.classList.add("answer")
+        buttonElement.setAttribute("data-correct", answer.correct)
+        buttonElement.textContent = answer.option
+        questionsButton.appendChild(buttonElement)
+        
+        document.querySelectorAll(".answer").forEach((button) => {
+            button.addEventListener("click", nextQuestion) 
+        })
+    })
+}
+loadQuestions()
