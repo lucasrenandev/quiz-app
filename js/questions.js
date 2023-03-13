6e5fedd23be679d899da3084a7d8448bfbfab8e6
lucasrenandns <lucasrenandns96@gmail.com>
2023-03-13 20:00:15 -0300

    New update quiz app

--- js/questions.js
+++ js/questions.js
@@ -9,6 +9,15 @@ export default [
         ]
     },
     {
+        question: "O que é HTML?",
+        answers: [
+            {option: "Linguagem de marcação", correct: true},
+            {option: "Linguagem de estilização", correct: false},
+            {option: "Linguagem de programação", correct: false},
+            {option: "Nenhuma das alternativas", correct: false},
+        ]
+    },
+    {
         question: "Qual o significado da sigla CSS?",
         answers: [
             {option: "Folhas de estilo em cascata", correct: true},
@@ -18,25 +27,34 @@ export default [
         ]
     },
     {
+        question: "O que é CSS?",
+        answers: [
+            {option: "Linguagem de programação", correct: false},
+            {option: "Linguagem de estilização", correct: true},
+            {option: "Linguagem de marcação", correct: false},
+            {option: "Nenhuma das alternativas", correct: true},
+        ]
+    },
+    {
         question: "Qual das alternativas abaixo não é considerada uma linguagem de programação?",
         answers: [
             {option: "C/C++", correct: false},
             {option: "Ruby/GO", correct: false},
             {option: "Html/Css", correct: true},
-            {option: "Php/Swift", correct: false},
+            {option: "PHP/Swift", correct: false},
         ]
     },
     {
         question: "Qual a linguagem de programação nativa dos Browsers(Navegadores Web)?",
         answers: [
             {option: "Java", correct: false},
-            {option: "Php", correct: false},
+            {option: "PHP", correct: false},
             {option: "Python", correct: false},
             {option: "JavaScript", correct: true},
         ]
     },
     {
-        question: "Defina o que é JavaScript?",
+        question: "O que é JavaScript?",
         answers: [
             {option: "Uma linguagem de programação client side(lado cliente)", correct: true},
             {option: "Uma linguagem de programação server side(lado servidor)", correct: false},
@@ -51,6 +69,33 @@ export default [
             {option: "Adicionar interatividade as páginas web", correct: true},
             {option: "Criar páginas web", correct: false},
             {option: "Adicionar estilo as páginas web", correct: false},
+        ]
+    },
+    {
+        question: "O que é um desenvolvedor Front-End?",
+        answers: [
+            {option: "Atua no lado client side", correct: true},
+            {option: "Atua no lado server side", correct: false},
+            {option: "Atua em ambos os lados", correct: false},
+            {option: "Nenhuma das alternativas", correct: false},
+        ]
+    },
+    {
+        question: "O que é um desenvolvedor Back-End?",
+        answers: [
+            {option: "Atua em ambos os lados", correct: false},
+            {option: "Atua no lado server side", correct: true},
+            {option: "Atua no lado client side", correct: false},
+            {option: "Nenhuma das alternativas", correct: false},
+        ]
+    },
+    {
+        question: "O que é um desenvolvedor Full Stack?",
+        answers: [
+            {option: "Atua no lado server side", correct: false},
+            {option: "Atua no lado client side", correct: false},
+            {option: "Atua em ambos os lados", correct: true},
+            {option: "Nenhuma das alternativas", correct: false},
         ]
     },
 ]
